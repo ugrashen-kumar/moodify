91b19c18539f330c3726fc5e1b5dad478afb146d
Ugrashen Kumar <ugrashen1992@gmail.com>
2026-06-22 17:49:47 +0530

    Fix production API URL

--- Frontend/src/features/auth/servises/auth.api.js
+++ Frontend/src/features/auth/servises/auth.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://moodify-7diw.onrender.com",
   withCredentials: true,
 });
 
